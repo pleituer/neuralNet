6fc833da0cef607b999eab82e4d9f5713002c0ed
P <73525539+pleituer@users.noreply.github.com>
2023-02-10 18:50:54 +0800

    Update neuralNet.py

--- neuralNet.py
+++ neuralNet.py
@@ -62,8 +62,46 @@ class Dense(Layer):
         if self.wantBias: self.bias -=learningRate*outputGradient
         return inputGradient
 
+#GRU
+#experimental (will break)
+class GRU(Layer):
+    def __init__(self, inputSize, outputSize, internalSize, internalFunc, D_internalFunc, trimFunc):
+        self.externalWeights = np.random.randn(outputSize, inputSize)
+        self.internalWeights = np.random.randn(outputSize, internalSize)
+        self.bias = np.random.randn(outputSize, internalSize)
+        self.value = np.random.randn(outputSize, 1)
+        self.internalFunc = internalFunc
+        self.D_internalFunc = D_internalFunc
+        self.trimFunc = trimFunc
+    
+    def forward(self, input):
+        self.input = input
+        self.value = np.dot(self.externalWeights, self.input) + self.internalFunc(self.internalWeights, self.value, self.bias)
+        return self.value
+    
+    def backward(self, outputGradient, learningRate):
+        externalWeightsGradient = np.dot(outputGradient, self.input.T)
+        inputGradient, internalWeightsGradient, biasGradient = self.D_internalFunc(outputGradient, self.externalWeights, self.internalWeights, self.value, self.bias)
+        self.externalWeights -=learningRate*externalWeightsGradient
+        self.internalWeights -=learningRate*internalWeightsGradient
+        self.bias -=learningRate*biasGradient
+        self.internalWeights = self.trimFunc(self.internalWeights)
+        return inputGradient
+
+#This is the newer version, a direct child from class GRU
 #Simplistic GRU, it is a GRU but with one internal weight and one internal activation (tanh)
-class SGRU(Layer):
+class SGRU(GRU):
+    def __init__(self, inputSize, outputSize):
+        internalSize = 1
+        def internalFunc(internalWeights, value, bias):
+            return np.multiply(self.internalWeights, self.value + self.bias)
+        def D_internalFunc(outputGradient, externalWeights, internalWeights, value, bias):
+            return np.dot(externalWeights.T, outputGradient), np.multiply(outputGradient, self.value), outputGradient
+        super().__init__(inputSize, outputSize, internalSize, internalFunc, D_internalFunc, tanh)
+
+#Simplistic GRU, it is a GRU but with one internal weight and one internal activation (tanh)
+#update: this has been remaned to OSGRU, this is the original version, a newer version has been made
+class OSGRU(Layer):
     def __init__(self, inputSize, outputSize):
         self.externalWeights = np.random.randn(outputSize, inputSize)
         self.internalWeights = np.random.randn(outputSize, 1)
@@ -202,6 +240,7 @@ class FFNN():
         self.network = network
     
     def train(self, X, Y, epochs, learningRate, ErrorFunc='MSE', test=True, testPercentage=0.9):
+        #split test and train data
         if test:
             trainX = X[:int(testPercentage*len(X))]
             testX = X[int(testPercentage*len(X)):]
@@ -210,32 +249,53 @@ class FFNN():
         else:
             trainX = X
             trainY = Y
+        #error functions
         errorFunc = Errorfunctions[ErrorFunc]
         D_errorFunc = D_Errorfunctions[ErrorFunc]
-        startTime = time.time()
+        #stores to plot
         trainingErrorPlot = ()
         testingErrorPlot = ()
+        #starts timeing
+        startTime = time.time()
+        #loops through each epoch
         for e in range(epochs):
+            #resets error
             error = 0
+            #loops through traingin data
             for x, y in zip(trainX,trainY):
+                #forward
                 output = x
                 for layer in self.network: output = layer.forward(output)
                 error += errorFunc(y, output)
+                #backward
                 gradient = D_errorFunc(y, output)
                 for layer in reversed(self.network): gradient = layer.backward(gradient, learningRate)
+            #normalize error
             error /= len(X)
+            #prints error
             print(f'Epoch {e+1}/{epochs}, training error: {error}, training accuracy: {1-error}')
+            #stores error
             trainingErrorPlot += (error,)
+            #resets error
             error = 0
+            #loops through testing data
             for x, y in zip(testX, testY):
+                #forward
                 output = x
                 for layer in self.network:output = layer.forward(output)
                 error += errorFunc(y, output)
+                #no backwards for test
+            #normalizes error
             error /= len(X)
+            #prints error
             print(f'Epoch {e+1}/{epochs}, testing error: {error}, testing accuracy: {1-error}')
+            #stores error
             testingErrorPlot += (error,)
+        #stop timing
         endTime = time.time()
+        #prints time
         print(f'Time spent: {endTime - startTime} seconds')
+        #plots
         plt.plot(np.linspace(0, epochs+1, num=epochs+1), (1,)+trainingErrorPlot, label="Training Error")
         plt.plot(np.linspace(0, epochs+1, num=epochs+1), [0]+[1-_ for _ in trainingErrorPlot], label="Training Accuracy")
         plt.plot(np.linspace(0, epochs+1, num=epochs+1), (1,)+testingErrorPlot, label="Testing Error")
@@ -245,43 +305,92 @@ class FFNN():
         plt.show()
     
     def visualize(self, X, Y, optionsDisplay=None):
+        #set to default
         if not optionsDisplay: optionsDisplay = ['Neuron ' + str(i) for i in range(self.network[-1].outputSize)]
+        #loops through visualizing data
         for x, y in zip(X, Y):
+            #forward
             output = x
             for layer in self.network: output = layer.forward(output)
             error = MSE(y, output)
-            sortedOutput = sorted([(optionsDisplay[_], output[_]) for _ in range(len(output))], key = (lambda x: x[1]), reverse=True)
+            #no backward
+            #for each looping, rearranges the neurons to that the one with the highest activation is on top
+            sortedOutput = sorted(zip(optionsDisplay, output), key = (lambda x: x[1]), reverse=True)
+            #prints
             print(f'Input: {[list(_) for _ in x]}')
             for j in range(len(sortedOutput)): print(f'Output: {sortedOutput[j][0]} ({sortedOutput[j][1][0]})\tExpected: {float(y[j][0])}')
             print('-------------------')
 
 #Recurrent Neural Networks
-#Still in progress, the test() function needs to be coded
 class RNN():
     def __init__(self, network):
         self.network = network
     
-    def train(self, X, YTrue, epochs, learningRate, ErrorFunc='MSE'):
+    def train(self, X, Y, epochs, learningRate, ErrorFunc='MSE', test=True, testPercentage=0.9):
+        #extracts test and train data
+        if test:
+            trainX = X[:int(testPercentage*len(X))]
+            testX = X[int(testPercentage*len(X)):]
+            trainY = Y[:int(testPercentage*len(Y))]
+            testY = Y[int(testPercentage*len(Y)):]
+        else:
+            trainX = X
+            trainY = Y
+        #error function
         errorFunc = Errorfunctions[ErrorFunc]
         D_errorFunc = D_Errorfunctions[ErrorFunc]
+        #stores errors to plot
+        trainErrorPlot = ()
+        testErrorPlot = ()
+        #starts timing
         startTime = time.time()
-        errorPlot = ()
         for e in range(epochs):
+            #loops through each epochs
+            #resets error
             error = 0
-            for x, yTrue in zip(X, YTrue):
+            #loops through each training data
+            for x, y in zip(trainX, trainY):
+                #loops through each timestep of the data
                 for t in range(len(x)):
+                    #forward
                     output = x[t]
                     for layer in self.network: output = layer.forward(output)
-                    error += errorFunc(yTrue[t], output)
-                    gradient = D_errorFunc(yTrue[t], output)
+                    error += errorFunc(y[t], output)
+                    #backwards
+                    gradient = D_errorFunc(y[t], output)
                     for layer in reversed(self.network): gradient = layer.backward(gradient, learningRate)
-            error /= (len(X)*len(X[0]))
-            errorPlot += (error,)
-            print(f'Epoch {e+1}/{epochs}, error: {error}, accuracy: {1-error}')
+            #normalizes error
+            error /= (len(trainX)*len(trainX[0]))
+            #stores error
+            trainErrorPlot += (error,)
+            #prints error
+            print(f'Epoch {e+1}/{epochs}, training error: {error}, training accuracy: {1-error}')
+            #resets it
+            error = 0
+            #loops through each testing data
+            for x, y in zip(testX, testY):
+                #loops through each timestep of the data
+                for t in range(len(x)):
+                    #forward
+                    output = x[t]
+                    for layer in self.network: output = layer.forward(output)
+                    error += errorFunc(y[t], output)
+                    #no backward for test
+            #normalizes error
+            error /= (len(testX)*len(testX[0]))
+            #stores error
+            testErrorPlot += (error, )
+            #prints error
+            print(f'Epoch {e+1}/{epochs}, testing error: {error}, testing accuracy: {1-error}')
+        #stops timing
         endTime = time.time()
+        #prints time
         print(f'Time spent: {endTime - startTime} seconds')
-        ErrorLine = plt.plot(np.linspace(0, epochs+1, num=epochs+1), (1,)+errorPlot, label="Error")
-        AccuracyLine = plt.plot(np.linspace(0, epochs+1, num=epochs+1), [0]+[1-_ for _ in errorPlot], label="Accuracy")
-        plt.legend(["Error", "Accuracy"])
+        #plots
+        plt.plot(np.linspace(0, epochs+1, num=epochs+1), (1,)+trainErrorPlot, label="Training Error")
+        plt.plot(np.linspace(0, epochs+1, num=epochs+1), [0]+[1-_ for _ in trainErrorPlot], label="Training Accuracy")
+        plt.plot(np.linspace(0, epochs+1, num=epochs+1), (1,)+testErrorPlot, label="Testing Error")
+        plt.plot(np.linspace(0, epochs+1, num=epochs+1), [0]+[1-_ for _ in testErrorPlot], label="Testing Accuracy")
+        plt.legend(["Training Error", "Training Accuracy", "Testing Error", "Testing Accuracy"])
         plt.xlabel("Epochs")
         plt.show()
